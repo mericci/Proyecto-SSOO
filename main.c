23da4cc2994048e282f76cad7afac8378dc9190b
mericci <mericci@uc.cl>
2019-10-04 18:23:43 -0300

    Makefile y +

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    if(argc < 2)
+	{
+		printf("Los parametros no fueron entregados de manera correcta");
+		return 1;
+	}
+}
